badc1de32f0545d7bb077eb513e39782776c64f5
Daniil Deshkovich <daniil.deshkovich@gmail.com>
2023-08-27 15:54:16 +0300

    Add bonus item logic

---
+++ Assets/Scripts/Game/Items/BonusItem.cs
@@ -0,0 +1,24 @@
+using ItemPicker.Services;
+using UnityEngine;
+
+namespace ItemPicker.Game.Items
+{
+    public class BonusItem : Item
+    {
+        #region Variables
+
+        [SerializeField] private int _count;
+
+        #endregion
+
+        #region Protected methods
+
+        protected override void PerformActions()
+        {
+            base.PerformActions();
+            GameService.Instance.ChangeHP(_count);
+        }
+
+        #endregion
+    }
+}
